dbeafba101dee1fbd39c1d8fe75536540d4a95b5
Gokul Kumar <gokulkumar0307@gmail.com>
2026-02-15 07:38:25 +0530

    shell script;git gin main

---
+++ 02.conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "RAMESH:: Hi Suresh, How are you"?
+echo "SURESH:: Hi Ramesh, i am fine. How are you?"
+echo "RAMESH:: What are you learning recently"
+echo "SURESH:: I am learning Shell Script Ramesh"
